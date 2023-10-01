cd6217dab3aca69ba7fefc67ce7fbae8c9b68ead
Carson5715 <97638211+Carson5715@users.noreply.github.com>
2023-10-01 16:22:52 -0400

    Carson Level Update

---
+++ PsychShift/Assets/Design Department/Design Test Scenes/Carson Only/Testing/Test Props/TestBreakObjectCode.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+public class TestBreakObjectCode : MonoBehaviour
+{
+    public GameObject fractured;
+    public GameObject objectToDestroy; // Add a public GameObject variable to specify the object to destroy
+    public float breakForce;
+    private bool Test = true;
+
+    private bool isDestroyed = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown("f") && !isDestroyed && Test == true){
+            BreakTheThing();
+            Test = false;
+        }
+
+    }
+
+    public void BreakTheThing()
+    {
+        GameObject frac = Instantiate(fractured, transform.position, transform.rotation);
+
+        foreach (Rigidbody rb in frac.GetComponentsInChildren<Rigidbody>())
+        {
+            Vector3 force = (rb.transform.position - transform.position).normalized * breakForce;
+            rb.AddForce(force);
+        }
+
+        // Destroy the specified object along with the original GameObject
+        if (objectToDestroy != null){
+            Destroy(objectToDestroy);
+            //Test = false;
+        }
+
+        // Wait for 5 seconds and then destroy the instantiated object
+        StartCoroutine(DestroyAfterDelay(frac, 5.0f));
+    }
+
+    private IEnumerator DestroyAfterDelay(GameObject obj, float delayInSeconds)
+    {
+        yield return new WaitForSeconds(delayInSeconds);
+        Destroy(obj);
+    }
+}
