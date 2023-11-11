8c9448de0356ff39953ed161d78a8e8e494d8ed0
MaxJones2003 <jones.max2003@gmail.com>
2023-11-11 18:53:15 -0500

    guns

--- PsychShift/Assets/Code Department/Scripts/NewGuns/Monobehaviours/PlayerAction.cs
+++ PsychShift/Assets/Code Department/Scripts/NewGuns/Monobehaviours/PlayerAction.cs
@@ -16,16 +16,13 @@ namespace Guns.Demo
         [SerializeField]
         private Animator PlayerAnimator;
         [SerializeField]
-        private Image Crosshair;
-        [SerializeField]
-        private Transform AimTarget;
         private bool IsReloading;
 
         private void Update()
         {
             GunSelector.ActiveGun.Tick(
                 !IsReloading
-                && Application.isFocused && Mouse.current.leftButton.isPressed
+                && Application.isFocused && InputManager.Instance.ShootAction.triggered
                 && GunSelector.ActiveGun != null
             );
 
@@ -33,55 +30,9 @@ namespace Guns.Demo
             {
                 GunSelector.ActiveGun.StartReloading();
                 IsReloading = true;
-                PlayerAnimator.SetTrigger("Reload");
+                //PlayerAnimator.SetTrigger("Reload");
                 InverseKinematics.HandIKAmount = 0.25f;
                 InverseKinematics.ElbowIKAmount = 0.25f;
-            }
-
-            UpdateCrosshair();
-        }
-
-        private void UpdateCrosshair()
-        {
-            Vector3 gunTipPoint = GunSelector.ActiveGun.GetRaycastOrigin();
-            Vector3 forward;
-            if (GunSelector.ActiveGun.ShootConfig.ShootType == ShootType.FromGun)
-            {
-                forward = GunSelector.ActiveGun.GetGunForward();
-            }
-            else
-            {
-                forward = GunSelector.Camera.transform.forward;
-            }
-
-            Vector3 hitPoint = gunTipPoint + forward * 10;
-            if (Physics.Raycast(gunTipPoint, forward, out RaycastHit hit, float.MaxValue, GunSelector.ActiveGun.ShootConfig.HitMask))
-            {
-                hitPoint = hit.point;
-            }
-
-            AimTarget.transform.position = hitPoint;
-
-            if (GunSelector.ActiveGun.ShootConfig.ShootType == ShootType.FromGun)
-            {
-                Vector3 screenSpaceLocation = GunSelector.Camera.WorldToScreenPoint(hitPoint);
-
-                if (RectTransformUtility.ScreenPointToLocalPointInRectangle(
-                    (RectTransform)Crosshair.transform.parent,
-                    screenSpaceLocation,
-                    null,
-                    out Vector2 localPosition))
-                {
-                    Crosshair.rectTransform.anchoredPosition = localPosition;
-                }
-                else
-                {
-                    Crosshair.rectTransform.anchoredPosition = Vector2.zero;
-                }
-            }
-            else
-            {
-                Crosshair.rectTransform.anchoredPosition = Vector2.zero;
             }
         }
 
