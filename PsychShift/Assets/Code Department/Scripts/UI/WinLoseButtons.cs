185f72babe88bf29c9b4731a1a8c89466dcd9924
Jemora2 <113477756+Jemora2@users.noreply.github.com>
2023-11-12 15:44:38 -0500

    Bar revamp and 2 more squares

---
+++ PsychShift/Assets/Code Department/Scripts/UI/WinLoseButtons.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class WinLoseButtons : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject WinUI;
+    public GameObject LoseUI;
+
+    void Start()
+    {
+        WinUI.SetActive(false);
+        LoseUI.SetActive(false);
+        Time.fixedDeltaTime = .02f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.L))
+        {
+            LoseUI.SetActive(true);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+        if (Input.GetKey(KeyCode.I))
+        {
+            WinUI.SetActive(true);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void Quit()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main Menu VS");
+    }
+}
