1ed366b830a6a7c33480756b98bfff4317a0155d
MaxJones2003 <jones.max2003@gmail.com>
2023-11-12 16:32:29 -0500

    added bullet spread

---
+++ PsychShift/Assets/Code Department/Scripts/GunScripts/NewGuns/Bullets/BulletSpreadType.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Guns
+{
+    public enum BulletSpreadType
+    {
+        Simple,
+        TextureBased
+    }
+
+}
