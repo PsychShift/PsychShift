64fbb64abafc364a4dbf68159e22fa54e3c18240
Jemora2 <113477756+Jemora2@users.noreply.github.com>
2023-11-28 18:29:47 -0500

    lose screen
    lose screen in code test scene 2

--- PsychShift/Assets/Code Department/Scripts/Enemy/Health/HealthUI.cs
+++ PsychShift/Assets/Code Department/Scripts/Enemy/Health/HealthUI.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class HealthUI : MonoBehaviour
 {
     public Slider healthBar;
+    public GameObject LoseUI;
 
 
 
@@ -18,9 +19,17 @@ public class HealthUI : MonoBehaviour
     {
         if(healthBar.value - damage < 0) damage = 0;
         healthBar.value -= damage;
+
+        if(healthBar.value == 0)
+        {
+            LoseUI.SetActive(true);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+        }
     }
     public void Enabled(bool enabled)
     {
         healthBar.enabled = enabled;
     }
+    
 }
