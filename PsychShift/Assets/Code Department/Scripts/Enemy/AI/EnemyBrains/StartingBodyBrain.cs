375ddc944dd2b4c04b8cb8c4034d759e48cfddee
MaxJones2003 <jones.max2003@gmail.com>
2023-11-29 18:05:48 -0500

    starting enemy

---
+++ PsychShift/Assets/Code Department/Scripts/Enemy/AI/EnemyBrains/StartingBodyBrain.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingBodyBrain : EnemyBrain
+{
+    public override void StateMachineSetup()
+    {
+        // We don't need a state machine for this enemy
+        // he do be a vegetable
+    }
+
+    protected override void SetUp()
+    {
+        // We don't need a state machine for this enemy
+        // he do be a vegetable
+    }
+}
