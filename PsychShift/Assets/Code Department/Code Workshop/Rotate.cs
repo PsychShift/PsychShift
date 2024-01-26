734213112bbb58c0942b2762c203e1a53deb7d34
Donovan Mercado <112048010+DMercado02@users.noreply.github.com>
2024-01-26 00:19:42 -0500

    Lobby Security V2

---
+++ PsychShift/Assets/Code Department/Code Workshop/Rotate.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingManipulatable : MonoBehaviour, IManipulate
+{
+    [SerializeField] private float rotationSpeed;
+    private float direction = 1f;
+
+    [SerializeField] private bool rotateX;
+    [SerializeField] private bool rotateY;
+    [SerializeField] private bool rotateZ;
+
+    [SerializeField]private bool canInteract = false;
+    public bool IsInteracted { get; set; }
+    public bool CanInteract { get { return canInteract; } set { canInteract = value; } }
+    private bool forward = true;
+
+    private void Awake() {
+        
+    }
+    private void Start() {
+        //StartCoroutine(WhileTest());
+    }
+    private void FixedUpdate() {
+        
+        direction = forward ? 1f : -1f;
+        direction *= rotationSpeed;
+        float x = rotateX ? 1 * direction: 0f;
+        float y = rotateY ? 1 * direction: 0f;
+        float z = rotateZ ? 1 * direction: 0f;
+        transform.Rotate(x,y,z,Space.Self);
+    }
+
+    public void Interacted()
+    {
+        Debug.Log("interacted");
+        if(CanInteract)
+            forward = !forward;
+    }
+
+    public void Interact()
+    {
+        
+    }
+
+    public void ReverseInteract()
+    {
+        
+    }
+
+    public IEnumerator WhileTest()
+    {
+        while(true)
+        {
+            Debug.Log("while");
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
+/*     private void OnEnable() {
+        
+    }
+    private void OnDisable() {
+        
+    }
+    private void OnDestroy() {
+        
+    } */
