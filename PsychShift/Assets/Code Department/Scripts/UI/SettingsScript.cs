b9815e48861138f76cac8f5d990fedda0e299fee
Jemora2 <113477756+Jemora2@users.noreply.github.com>
2024-02-02 21:41:26 -0500

    Resolution
    thats about it

--- PsychShift/Assets/Code Department/Scripts/UI/SettingsScript.cs
+++ PsychShift/Assets/Code Department/Scripts/UI/SettingsScript.cs
@@ -2,38 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class SettingsScript : MonoBehaviour
 {
-    public Dropdown resolutionDropdown;
+    //public Dropdown resolutionDropdown;
     public Toggle vsyncTog;
 
-    Resolution[] resolutions;
+    //Resolution[] resolutions;
 
     // Start is called before the first frame update
     void Start()
     {
-        resolutions = Screen.resolutions;
+        //resolutions = Screen.resolutions;
 
-        resolutionDropdown.ClearOptions();
+        //resolutionDropdown.ClearOptions();
 
-        List<string> options = new List<string>();
+       // List<string> options = new List<string>();
 
-        int currentResolutionIndex = 0;
-        for (int i = 0; i < resolutions.Length; i++)
-        {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
+        //int currentResolutionIndex = 0;
+       // for (int i = 0; i < resolutions.Length; i++)
+        //{
+            //string option = resolutions[i].width + " x " + resolutions[i].height;
+            //options.Add(option);
 
-            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolutionIndex = i;
-            }
-        }
+            //if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            //{
+                //currentResolutionIndex = i;
+            //}
+        //}
 
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolutionIndex;
-        resolutionDropdown.RefreshShownValue();
+        //resolutionDropdown.AddOptions(options);
+        //resolutionDropdown.value = currentResolutionIndex;
+        //resolutionDropdown.RefreshShownValue();
         if (QualitySettings.vSyncCount == 0)
         {
             vsyncTog.isOn = false;
@@ -60,11 +61,11 @@ public class SettingsScript : MonoBehaviour
         }
     }
 
-    public void SetResolution (int resolutionIndex)
-    {
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
-    }
+    //public void SetResolution (int resolutionIndex)
+   // {
+        //Resolution resolution = resolutions[resolutionIndex];
+        //Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    //}
 
     public void SetQuality(int qualityIndex)
     {
