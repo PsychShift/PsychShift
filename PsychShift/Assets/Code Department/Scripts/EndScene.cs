62bcdfc855b5792a0462be8b2e1b84bda2335e9b
KevS003 <kjscordero@gmail.com>
2024-02-04 22:52:13 -0500

    C reset

--- PsychShift/Assets/Code Department/Scripts/EndScene.cs
+++ PsychShift/Assets/Code Department/Scripts/EndScene.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using Player;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class EndScene : MonoBehaviour
 {
+    public PlayerStateMachine checkpointRest;
     private void OnTriggerEnter(Collider other) 
     {
         if(other.tag == "Player")
-            SceneManager.LoadScene("Outro");     
+        {
+            SceneManager.LoadScene("Outro"); 
+            checkpointRest.PleaseSetLocationGODPLEASE(this.transform, true);
+        }
+            
+
     }
 }
--- PsychShift/Assets/keyc.cs
+++ PsychShift/Assets/keyc.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using Player;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class keyc : MonoBehaviour
 {
     // Start is called before the first frame update
+    public PlayerStateMachine checkpointRest;
     void Awake()
     {
         SceneManager.LoadScene("WinScreen");
+        checkpointRest.PleaseSetLocationGODPLEASE(this.transform, true);
     }
 }
