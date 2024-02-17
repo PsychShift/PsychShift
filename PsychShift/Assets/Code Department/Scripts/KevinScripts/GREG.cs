1f210bab55a0d40e2f264edcdac5deaa68b13dbc
KevS003 <kjscordero@gmail.com>
2024-02-16 21:26:10 -0500

    Greg new

---
+++ PsychShift/Assets/Code Department/Scripts/KevinScripts/GREG.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GREG : MonoBehaviour, IDamageable
+{
+    // Start is called before the first frame update
+    
+    private bool wasHit => CurrentHealth == 0;
+
+    public int CurrentHealth {get;set;}
+
+    public int MaxHealth { get { return 1; } }
+
+    public bool IsWeakPoint {get;set;}
+
+    public event IDamageable.TakeDamageEvent OnTakeDamage;
+    public event IDamageable.DeathEvent OnDeath;
+    public GameObject[] rooms; 
+    private int currentRoomIndex;
+
+    private void Awake() {
+        CurrentHealth = MaxHealth;
+    }
+    public void TakeDamage(int Damage)
+    {
+        ShootHitScan();
+    }
+
+     public void ShootHitScan()
+    { 
+
+        rooms[currentRoomIndex].SetActive(false);
+        currentRoomIndex = (currentRoomIndex+1)%rooms.Length;
+        rooms[currentRoomIndex].SetActive(enabled);
+    } 
+    
+
+
+}
--- PsychShift/Assets/Design Department/LAB LEVELS/Tomas Bullshit/Portal.cs
+++ PsychShift/Assets/Design Department/LAB LEVELS/Tomas Bullshit/Portal.cs
@@ -7,11 +7,26 @@ public class teleport : MonoBehaviour
     public Transform player, destination;
  public GameObject playerg;
  
- void OnTriggerEnter(Collider other){
+ /* void OnTriggerEnter(Collider other){
   if(other.CompareTag("Player")){
    playerg.SetActive(false);
    player.position = destination.position;
    playerg.SetActive(true);
   }
+ } */
+
+ void OnTriggerEnter(Collider other)
+ {
+    if(other.CompareTag("Player"))
+    {
+        CharacterController playerC=other.GetComponent<CharacterController>();
+        playerC.enabled = false;
+        GameObject playerObj = other.GetComponent<GameObject>();
+        Transform playerPosition = playerObj.GetComponent<Transform>();
+        playerPosition = destination;
+        playerC.enabled = true;
+        gameObject.SetActive(false);
+        
+    }
  }
 }
