5c6d84a26899199db9950901e72be8604b09d8db
Carson5715 <97638211+Carson5715@users.noreply.github.com>
2024-02-20 12:30:14 -0500

    carson cooked

---
+++ PsychShift/Assets/Design Department/TESTING/MISC/Testing Code/AudioWEnableObject.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Collections;
+using UnityEngine;
+
+public class AudioWEnableObject : MonoBehaviour
+{
+    // GameObject to enable/disable
+    public GameObject objectToToggle;
+
+    // Time to disable the object
+    public float disableTime = 2.0f;
+
+    private bool hasBeenTriggered = false;
+
+    // This method is called when any collider enters the Box Collider
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the trigger has already been used
+        if (!hasBeenTriggered)
+        {
+            // Enable the object if it's not null
+            if (objectToToggle != null)
+            {
+                objectToToggle.SetActive(true);
+
+                // Start coroutine to disable the object after a certain time
+                StartCoroutine(DisableObjectAfterDelay());
+            }
+
+            // Disable the trigger
+            hasBeenTriggered = true;
+        }
+    }
+
+    // Disable the object after a certain delay
+    private IEnumerator DisableObjectAfterDelay()
+    {
+        // Wait for the specified time
+        yield return new WaitForSeconds(disableTime);
+
+        // Disable the object
+        if (objectToToggle != null)
+        {
+            objectToToggle.SetActive(false);
+        }
+    }
+}
