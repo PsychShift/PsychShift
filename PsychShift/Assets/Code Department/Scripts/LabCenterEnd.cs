0f7bd47ee8c1021539ede29de34fc83cabaf588e
Jemora2 <113477756+Jemora2@users.noreply.github.com>
2024-02-25 17:30:39 -0500

    midas cutscene

--- PsychShift/Assets/Code Department/Scripts/LabCenterEnd.cs
+++ PsychShift/Assets/Code Department/Scripts/LabCenterEnd.cs
@@ -5,10 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class LabCenterEnd : MonoBehaviour
 {
+    public GameObject MidasVideo;
+    public GameObject OFCanvas;
+    public GameObject Player;
+    private Coroutine WaitforEnd;
     // Start is called before the first frame update
     void Start()
     {
-        
+        MidasVideo.SetActive(false);
     }
 
     // Update is called once per frame
@@ -21,10 +25,30 @@ public class LabCenterEnd : MonoBehaviour
         if(other.tag == "Player")
         {
             PlayerMaster.Instance.StartNew();
-            SceneManager.LoadScene("FINAL BOSS"); 
+            //SceneManager.LoadScene("FINAL BOSS");
+            WaitforEnd = StartCoroutine(CutScene());
+            MidasVideo.SetActive(true);
+            OFCanvas.SetActive(false);
+            //Player.SetActive(false);
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+            Time.timeScale = 0f;
+
 
         }
-            
-
+    }
+    private IEnumerator CutScene()
+    {
+        yield return new WaitForSeconds(45);
+        Time.timeScale = 1f;
+        OFCanvas.SetActive(true);
+        SceneManager.LoadScene("FINAL BOSS");
+        
+    }
+    public void Skip()
+    {
+        Time.timeScale = 1f;
+        OFCanvas.SetActive(true);
+        SceneManager.LoadScene("FINAL BOSS");
     }
 }
