0026e2164e28f938184b9eb280909b1c1bbc3f42
KevS003 <kjscordero@gmail.com>
2024-03-05 15:49:07 -0500

    PuzzleKit improvement

---
+++ PsychShift/Assets/Code Department/Scripts/KevinScripts/Classified.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classified : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.LogError("I'm in your walls");
+        Debug.Log("I'm in your walls");
+        //Debug.LogException("I'm in your walls");
+    }
+}
