5947fec3b2b1c0a0bb1a29638473f133f573a04f
Carson5715 <97638211+Carson5715@users.noreply.github.com>
2024-03-16 21:48:10 -0400

    sad boi times

--- PsychShift/Assets/Code Department/Scripts/Enemy/GeneralScripts/Outliner.cs
+++ PsychShift/Assets/Code Department/Scripts/Enemy/GeneralScripts/Outliner.cs
@@ -9,30 +9,30 @@ public class Outliner : MonoBehaviour
     [SerializeField] private Color highlightColor = Color.blue; // Set the color you want when the player hovers over the object
     private Color nonHighlightColor;
 
-    private void Awake()
-    {
+   // private void Awake()
+   // {
         // Get the outline material attached to the character
-        Material[] materials = null;
-        if(UsesSkinnedMeshRenderer)
-        {
-            materials = GetComponentInChildren<SkinnedMeshRenderer>().materials;
-            outlineMaterial = materials[5];
-        }
-        else
-        {
-            materials = GetComponentInChildren<Renderer>().materials;
-            outlineMaterial = materials[0];
-        }
+     //   Material[] materials = null;
+      //  if(UsesSkinnedMeshRenderer)
+      //  {
+     //       materials = GetComponentInChildren<SkinnedMeshRenderer>().materials;
+      //      outlineMaterial = materials[5];
+     //   }
+      //  else
+     //   {
+     //       materials = GetComponentInChildren<Renderer>().materials;
+     //       outlineMaterial = materials[0];
+     //   }
 
         // Store the original color
-        nonHighlightColor = outlineMaterial.GetColor("_Outline_Color");
-    }
+      //  nonHighlightColor = outlineMaterial.GetColor("_Outline_Color");
+   // }
 
     public void ActivateOutline(bool isActive)
     {
-        if(isActive)
-            outlineMaterial.SetColor("_Outline_Color", highlightColor);
-        else
-            outlineMaterial.SetColor("_Outline_Color", nonHighlightColor);
+        //if(isActive)
+          //  outlineMaterial.SetColor("_Outline_Color", highlightColor);
+        //else
+         //   outlineMaterial.SetColor("_Outline_Color", nonHighlightColor);
     }
 }
