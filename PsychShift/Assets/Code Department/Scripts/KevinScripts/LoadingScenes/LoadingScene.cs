60062e65f5c788e90506a96469012088ad7b823e
KevS003 <kjscordero@gmail.com>
2024-03-23 22:57:00 -0400

    Push

--- PsychShift/Assets/Code Department/Scripts/KevinScripts/LoadingScenes/LoadingScene.cs
+++ PsychShift/Assets/Code Department/Scripts/KevinScripts/LoadingScenes/LoadingScene.cs
@@ -3,15 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+using UnityEngine.InputSystem;
 
 public class LoadingScene : MonoBehaviour
 {
+    public GameObject mainCamera;
     public GameObject LoadingScreen;
     public Image LoadingBarFill;
+    public GameObject cutSceneCanvas;
+    private bool cutSceneSkipped;
+    private int sceneIDRef;
+    public PlayerInput playerInput;
+    //static int NextScene;
     // Start is called before the first frame update
     public void LoadScene(int sceneId)
     {
-        StartCoroutine(LoadSceneAsync(sceneId));
+        sceneIDRef=sceneId;
+        Time.timeScale = 0f;
+        playerInput.enabled= true;
+        if(videoPlayer!=null)
+        {
+            cutSceneCanvas.SetActive(true);
+            StartCoroutine(LoadSceneVideo(sceneId));
+        }
+            
+        else
+            StartCoroutine(LoadSceneAsync(sceneId));
     }
     IEnumerator LoadSceneAsync(int sceneId)
     {
@@ -25,5 +43,36 @@ public class LoadingScene : MonoBehaviour
             //LoadingBarFill.fillAmount = progressValue;
             yield return null;
         }
+    }
+
+    public VideoPlayer videoPlayer;
+    
+    //public bool isCutScene;
+   
+    IEnumerator LoadSceneVideo(int sceneId)
+    {
+        videoPlayer.Play();
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneId);
+        operation.allowSceneActivation = false;
+        
+        
+
+        while(!operation.isDone && videoPlayer.isPlaying)
+        {
+            //float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+            if(cutSceneSkipped && operation.isDone)
+            {
+                videoPlayer.Stop();
+                operation.allowSceneActivation = true;
+            }
+            //LoadingBarFill.fillAmount = progressValue;
+            yield return null;
+        }
+        operation.allowSceneActivation = true;
+    }
+    public void SkipCutscene()
+    {
+        if(videoPlayer!=null)
+            SceneManager.LoadScene(sceneIDRef);
     }
 }
