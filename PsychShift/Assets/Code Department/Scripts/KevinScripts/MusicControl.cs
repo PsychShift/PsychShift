fe8747e15a036157750f2cd62a1ce1df187de3cb
KevS003 <kjscordero@gmail.com>
2024-04-04 22:49:24 -0400

    plez no hands plez

--- PsychShift/Assets/Code Department/Scripts/KevinScripts/MusicControl.cs
+++ PsychShift/Assets/Code Department/Scripts/KevinScripts/MusicControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class MusicControl : MonoBehaviour
 {
@@ -10,6 +11,16 @@ public class MusicControl : MonoBehaviour
     AudioClip Song;
     public bool isVA;
     public bool activated;
+    //public AudioMixer music;
+    public AudioMixer dialouge;
+    float currentVal;
+    float newVal;
+    public float numberDividedBy=2;
+    public MusicVolumeManager stopTheSlide;
+
+    //divide in half 
+    //save ref to OG value 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +29,18 @@ public class MusicControl : MonoBehaviour
     private void TrackChoice()
     {
         ChangeTrack(Song);
+        if(isVA)
+        {
+            Debug.Log("ISVA HERE");
+            currentVal = PlayerPrefs.GetFloat("musicVolume");
+            //newVal = currentVal/numberDividedBy;
+            //music.SetFloat("SFXVolume", currentVal/numberDividedBy);
+            //PlayerPrefs.SetFloat("musicVolume", currentVal/numberDividedBy);
+            stopTheSlide.musicvolumeSlider.value = currentVal/numberDividedBy;
+            stopTheSlide.playingDialouge = true;
+            StartCoroutine(waitForSound());
+        }
+        this.gameObject.GetComponent<Collider>().enabled = false;
     }
     private void ChangeTrack(AudioClip song)
     {
@@ -29,11 +52,21 @@ public class MusicControl : MonoBehaviour
         if(other.gameObject.tag == "Player")
         {
             TrackChoice();
-            if(isVA)
-                Destroy(gameObject);
-        }
-            
+            /* if(isVA)
+                Destroy(gameObject); */
+        }   
+    }
 
-            
+    IEnumerator waitForSound()
+    {
+        Debug.Log("InCoRoutine");
+        stopTheSlide.ChangeVolume();
+        yield return new WaitWhile (()=> musicPlayer.isPlaying);
+        //music.SetFloat("SFXVolume", currentVal);
+        //PlayerPrefs.SetFloat("musicVolume", currentVal);
+        stopTheSlide.musicvolumeSlider.value = currentVal;
+        stopTheSlide.ChangeVolume();
+        stopTheSlide.playingDialouge = false;
+        Debug.Log("Out");
     }
 }
