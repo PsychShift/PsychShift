d2408f99874fc6c08520fca0d54003014b1f843d
Jemora2 <113477756+Jemora2@users.noreply.github.com>
2024-04-22 21:14:17 -0400

    easy script

---
+++ PsychShift/Assets/Code Department/Scripts/UI/CreditsScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsScript : MonoBehaviour
+{
+    public GameObject CreditsObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1f;
+        CreditsObject.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //CreditsObject.SetActive(true);
+    }
+}
